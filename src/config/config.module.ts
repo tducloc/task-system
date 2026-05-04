a1d7244533f0b4c8878f5c03f8f799d0d19ebf63
Loc Tran <loctd@ztolabs.com>
2026-05-04 21:21:41 +0700

    init project

---
+++ src/config/config.module.ts
@@ -0,0 +1,11 @@
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
+
+ConfigModule.forRoot({
+  isGlobal: true,
+  validationSchema: Joi.object({
+    DATABASE_URL: Joi.string().required(),
+    JWT_SECRET: Joi.string().required(),
+    JWT_REFRESH_SECRET: Joi.string().required(),
+  }),
+});
