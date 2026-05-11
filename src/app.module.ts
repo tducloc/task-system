ae76ca78591a6b05ee46387dd89e1d694570fda4
Loc Tran <loctd@ztolabs.com>
2026-05-11 21:06:32 +0700

    feat: Add global auth guards and public decorator

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 
 import { PrismaModule } from './database/prisma.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { AuthGuard } from './modules/auth/guards/auth.guard';
 import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [PrismaModule, UsersModule, AuthModule],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
